0e9b7cf5256be89956655cfd970ec64cf245ae13
Yulia Selivanova <selivanyulia@mail.ru>
2023-09-06 23:34:42 +0300

    fix: fix configs

--- backend/ecosystem.config.js
+++ backend/ecosystem.config.js
@@ -16,7 +16,7 @@ module.exports = {
       user: DEPLOY_USER,
       host: DEPLOY_HOST,
       ref: DEPLOY_REF,
-      repo: 'https://github.com/seliveren/web-plus-pm2-deploy.git',
+      repo: 'git@github.com:seliveren/web-plus-pm2-deploy.git',
       path: DEPLOY_PATH,
       'pre-deploy': `scp ./*.env.deploy ${DEPLOY_USER}@${DEPLOY_HOST}:${DEPLOY_PATH}/current/backend`,
       'post-deploy': `cd ${DEPLOY_PATH}/current/backend && npm i && npm run build && pm2 startOrRestart ecosystem.config.js`,
