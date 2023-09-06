8909067c5a263b9166347322709560d4ea19df6d
Yulia Selivanova <selivanyulia@mail.ru>
2023-09-06 23:03:24 +0300

    feat: add configs

---
+++ backend/ecosystem.config.js
@@ -0,0 +1,25 @@
+require('dotenv').config({path: './.env.deploy'});
+
+const {
+  DEPLOY_USER, DEPLOY_HOST, DEPLOY_PATH, DEPLOY_REPO, DEPLOY_REF = 'origin/master',
+} = process.env;
+
+module.exports = {
+  apps: [{
+    name: 'backend',
+    script: './dist/app.js',
+  }],
+
+  // Настройка деплоя
+  deploy: {
+    production: {
+      user: DEPLOY_USER,
+      host: DEPLOY_HOST,
+      ref: DEPLOY_REF,
+      repo: DEPLOY_REPO,
+      path: DEPLOY_PATH,
+      'pre-deploy': `scp ./*.env.deploy ${DEPLOY_USER}@${DEPLOY_HOST}:${DEPLOY_PATH}/current/backend`,
+      'post-deploy': `cd ${DEPLOY_PATH}/current/backend && npm i && npm run build && pm2 startOrRestart ecosystem.config.js`,
+    },
+  },
+};
---
+++ frontend/ecosystem.config.js
@@ -0,0 +1,19 @@
+require('dotenv').config({path: './env.deploy'});
+
+const {
+  DEPLOY_USER, DEPLOY_HOST, DEPLOY_PATH,DEPLOY_REPO,  DEPLOY_REF = 'origin/master',
+} = process.env;
+
+module.exports = {
+  deploy: {
+    production: {
+      user: DEPLOY_USER,
+      host: DEPLOY_HOST,
+      ref: DEPLOY_REF,
+      repo: DEPLOY_REPO,
+      path: DEPLOY_PATH,
+      'pre-deploy': `scp ./*.env ${DEPLOY_USER}@${DEPLOY_HOST}:${DEPLOY_PATH}`,
+      'post-deploy': `cd ${DEPLOY_PATH}/current/frontend && npm i && npm run build`,
+    },
+  },
+};
