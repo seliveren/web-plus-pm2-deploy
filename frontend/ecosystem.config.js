fc9c1a207e066f339c313f508a6536e56dd40e49
seliveren <selivanyulia@mail.ru>
2023-09-07 00:26:10 +0300

    fix: fix configs

--- frontend/ecosystem.config.js
+++ frontend/ecosystem.config.js
@@ -12,7 +12,7 @@ module.exports = {
       ref: DEPLOY_REF,
       repo: 'https://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@github.com/seliveren/web-plus-pm2-deploy.git',
       path: DEPLOY_PATH,
-      'pre-deploy': `scp -i id_ed25519  ./*.env.deploy ${DEPLOY_USER}@${DEPLOY_HOST}:${DEPLOY_PATH}`,
+      'pre-deploy': `scp -i id_ed25519.pub  ./*.env.deploy ${DEPLOY_USER}@${DEPLOY_HOST}:${DEPLOY_PATH}`,
       'post-deploy': `cd ${DEPLOY_PATH}/current/frontend && npm i && npm run build`,
     },
   },
