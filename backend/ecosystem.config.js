7d996b1ee181963b75b82355ade94fb05ca079d1
seliveren <selivanyulia@mail.ru>
2023-09-07 15:03:38 +0300

    fix: fix config

--- backend/ecosystem.config.js
+++ backend/ecosystem.config.js
@@ -18,7 +18,7 @@ module.exports = {
       repo: 'https://github.com/seliveren/web-plus-pm2-deploy.git',
       path: DEPLOY_PATH,
       'pre-deploy-local': `scp ./.env ${DEPLOY_USER}@${DEPLOY_HOST}:${DEPLOY_PATH}/current/backend`,
-      'post-deploy': `cd ${DEPLOY_PATH}/current/backend && nvm install 14 && nvm use 14 && npm ci && npm run build && pm2 startOrRestart ecosystem.config.js --env production`,
+      'post-deploy': `cd ${DEPLOY_PATH}/current/backend && curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/master/install.sh | bash && nvm install 14 && nvm use 14 && npm ci && npm run build && pm2 startOrRestart ecosystem.config.js --env production`,
     },
   },
 };
